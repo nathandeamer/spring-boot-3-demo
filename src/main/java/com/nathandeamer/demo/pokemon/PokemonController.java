2c9e13aa5f94abc0001cf9e9113cb6a75cbf6ca7
nathandeamer <nd@nathandeamer.com>
2022-11-08 16:12:23 +0000

    Added Spring Docs (haven't tested native build yet)

--- src/main/java/com/nathandeamer/demo/pokemon/PokemonController.java
+++ src/main/java/com/nathandeamer/demo/pokemon/PokemonController.java
@@ -1,5 +1,12 @@
 package com.nathandeamer.demo.pokemon;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,14 +18,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "/api/v1")
+@Tag(name = "Pokemon", description = "The Pokemon API")
 public class PokemonController {
 
     private final PokemonService pokemonService;
 
-    @GetMapping(path = "/pokemon")
-    public PokemonDTO get(@RequestParam String name) {
+
+
+    @Operation(summary = "Get a Pokemon by Name", description = "Get a Pokemon by Name")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation",
+                    content = @Content(schema = @Schema(implementation = PokemonDTO.class)))
+    })
+    @GetMapping(path = "/pokemon", produces = { "application/json" })
+    public PokemonDTO get(@Parameter(description = "Name of pokemon", required = true) @RequestParam String name) {
         log.info("Getting pokemon by name: {}", name);
         return pokemonService.getPokemonByName(name);
     }
-
 }
--- src/main/java/com/nathandeamer/demo/pokemon/PokemonDTO.java
+++ src/main/java/com/nathandeamer/demo/pokemon/PokemonDTO.java
@@ -1,20 +1,26 @@
 package com.nathandeamer.demo.pokemon;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class PokemonDTO {
 
+    @Schema(description = "Pokemon ID", example = "151")
     int id;
 
+    @Schema(description = "Pokemon Name", example= "mew")
     String name;
 
+    @Schema(description = "Pokemon Base Experience", example= "300")
     @JsonProperty("base_experience")
     int baseExperience;
 
+    @Schema(description = "Pokemon Weight", example = "40")
     int weight;
 
+    @Schema(description = "Pokemon Height", example = "4")
     int height;
 
 }
